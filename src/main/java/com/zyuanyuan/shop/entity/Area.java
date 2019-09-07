4f934e2299b38c186ff0ee9d0b2640fd5e48dd99
zoyoy1203 <2071686148@qq.com>
2019-09-07 22:14:58 +0800

    校园商铺后台正式开始撸代码啦

---
+++ src/main/java/com/zyuanyuan/shop/entity/Area.java
@@ -0,0 +1,18 @@
+package com.zyuanyuan.shop.entity;
+
+import com.sun.scenario.effect.impl.prism.PrImage;
+
+import java.util.Date;
+
+public class Area {
+    //ID
+    private Integer areaId;
+    //名称
+    private String areaName;
+    //权重
+    private Integer priority;
+    //创建时间
+    private Date createTime;
+    //更新时间
+    private Date lastEditTime;
+}
